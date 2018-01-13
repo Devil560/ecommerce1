619d0ecbdc2a9107071d061ab3e59c27c81b1cf5
Devil560 <sanchit560@gmail.com>
2018-01-13 14:40:49 +0530

    Update login.php

--- login.php
+++ login.php
@@ -10,14 +10,22 @@ if (isset($_SESSION['email'])) {
 <html lang="en">
 
     <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="img/commerce.ico">
+        <title>Login|Lifestyle Store</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+          <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
 
-        <title>Login | Life Style Store</title>
+        <link href="style.css" rel="stylesheet" type="text/css">
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-        <link href="css/style.css" rel="stylesheet">
-        <script src="js/jquery.js"></script>
-        <script src="js/bootstrap.min.js"></script>
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
     </head>
 
     <body>
