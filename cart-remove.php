17769bc726f9deb4bdf87b520b678de41d5db3b3
Devil560 <sanchit560@gmail.com>
2018-01-13 14:23:34 +0530

    Update cart-remove.php

--- cart-remove.php
+++ cart-remove.php
@@ -1,6 +1,6 @@
 <?php
 
-require("includes/common.php");
+require("common.php");
 if (isset($_GET['id']) && is_numeric($_GET['id'])) {
     $item_id = $_GET["id"]; 
     $user_id = $_SESSION['user_id'];
